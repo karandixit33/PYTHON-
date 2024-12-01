8e206cae7fa591ddd215e2e0c8a37bd49fb3a5cf
i.karandixit <140276075+karandixit33@users.noreply.github.com>
2024-12-01 18:47:03 +0530

    Add files via upload

---
+++ weight measurement.py
@@ -0,0 +1,13 @@
+#weight measurement program in pyhton 
+
+weight = int(input("weight :"))
+unit = input("(K)g or (L)bs:")
+
+if unit.upper() == "K":
+    converted = weight/0.45
+    print("weight in Lbs:"+  str(converted))
+    
+else :
+    converted = weight * 0.45 
+    print ("weight in kgs:" + str(converted) )    
+    
