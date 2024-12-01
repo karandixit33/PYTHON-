7df89260c81f315ede4680edf441aadc56dc0f10
i.karandixit <140276075+karandixit33@users.noreply.github.com>
2024-12-01 18:47:59 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,12 @@
+# list in python
+
+Names = ["karan", "vishal", "devansh", "shrikanth"]
+#print(Names [3])
+#print(Names [-3])                            # starts from end of the list element
+
+Names [0] = "John"
+
+print  ( Names [0:3])
+
+print(Names)
+ 
