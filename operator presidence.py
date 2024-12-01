acfba5ec571dd557daca69959308d9db732205e5
i.karandixit <140276075+karandixit33@users.noreply.github.com>
2024-12-01 18:48:21 +0530

    Add files via upload

---
+++ operator presidence.py
@@ -0,0 +1,12 @@
+
+x = 10 + 3 * 2
+print(x)
+#output is 16
+
+
+x = (10+3)*2
+print(x)
+#output is 26 
+
+# mathmatical calculation in pyhton 
+# operator presidence in important to understand while code 
