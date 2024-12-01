b631db2fa31525925953305f980f7c1df2167670
i.karandixit <140276075+karandixit33@users.noreply.github.com>
2024-12-01 18:47:24 +0530

    Add files via upload

---
+++ type conversion.py
@@ -0,0 +1,11 @@
+#birth_year = input("enter your birth year")
+#age = 2024 - int(birth_year)
+#print(age)
+
+# sum of two numbers using type conversion 
+
+first = input("first: ")
+second = input("second: ")
+sum = float(first) + float (second)
+strsum = str(sum)
+print("sum:", strsum)
